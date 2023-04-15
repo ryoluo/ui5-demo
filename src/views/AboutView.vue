fda0d340d0fadd62c32c1dfd7a0725c16025ba8a
ryoluo <40686845+ryoluo@users.noreply.github.com>
2023-04-15 19:17:10 +0900

    First commit of full repository

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <div class="about">
+      <ui5-button>Hello world!</ui5-button>
+    </div>
+  </main>
+</template>
+
+<script>
+import "@ui5/webcomponents/dist/Button.js";
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
